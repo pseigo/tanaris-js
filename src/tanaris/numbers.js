84d168df324e0bd4a021b50b65c4c60ae43b8a42
Peyton Seigo <pseigo@yahoo.com>
2025-06-27 06:54:23 -0600

    [feat] Add more number predicates

--- src/tanaris/numbers.js
+++ src/tanaris/numbers.js
@@ -12,7 +12,31 @@
  */
 
 /**
- * Returns `true` iff `value` is a number or `BigInt`.
+ * Returns `true` iff `value` is a finite number, +/- `Infinity`, or `NaN`.
+ *
+ * @category Predicates
+ *
+ * @param {any} value
+ *
+ * @returns {boolean}
+ */
+export function isNumberLike(value) {
+  return typeof value === "number" || isBigInt(value);
+}
+
+/**
+ * Returns `true` iff `value` is a finite number.
+ *
+ * ## Special cases
+ *
+ * ### Includes
+ *
+ * - `BigInt`
+ *
+ * ### Excludes
+ *
+ * - `NaN`
+ * - +/- `Infinity`
  *
  * @category Predicates
  *
@@ -21,17 +45,28 @@
  * @returns {boolean}
  */
 export function isNumber(value) {
-  return typeof value === "number" || isBigInt(value);
+  return (
+    (typeof value === "number" && Number.isFinite(value)) || isBigInt(value)
+  );
 }
 
 /**
- * Returns `true` iff `value` is an integer or `BigInt`.
+ * Returns `true` iff `value` is an integer, or a floating point number that
+ * can be represented as an integer.
+ *
+ * ## Special cases
+ *
+ * ### Includes
+ *
+ * - `BigInt`
  *
  * @category Predicates
  *
  * @param {any} value
  *
  * @returns {boolean}
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#description
  */
 export function isInteger(value) {
   return Number.isInteger(value) || isBigInt(value);
@@ -48,4 +83,61 @@ export function isInteger(value) {
  */
 export function isBigInt(value) {
   return typeof value === "bigint";
+}
+
+/**
+ * Returns `true` iff `value` is finite. That is, a number or integer
+ * (including `BigInt`s) that's not `NaN` or +/= `Infinity`.
+ *
+ * @category Predicates
+ *
+ * @param {any} value
+ *
+ * @returns {boolean}
+ */
+export function isFinite(value) {
+  return Number.isFinite(value) || isBigInt(value);
+}
+
+/**
+ * Returns `true` iff `value` is +/= `Infinity`.
+ *
+ * ## Special cases
+ *
+ * ### Excludes
+ *
+ * - `NaN`
+ *
+ * @category Predicates
+ *
+ * @param {any} value
+ *
+ * @returns {boolean}
+ */
+export function isInfinite(value) {
+  return value === Infinity || value === -Infinity;
+}
+
+/**
+ * Returns `true` iff `value` is equivalent to `NaN`.
+ *
+ * Always use this function for comparisons involving `NaN` instead of `==` or
+ * `===`, because those operators always return false if one of the operands is
+ * `NaN`.
+ *
+ * Non number-like values will always return `false`, even if those values
+ * ordinarily become `NaN` when coerced to a number.
+ *
+ * Equivalent to built-in `Number.isNaN(value)`.
+ *
+ * @category Predicates
+ *
+ * @param {any} value
+ *
+ * @returns {boolean}
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN
+ */
+export function isNaN(value) {
+  return Number.isNaN(value);
 }
--- test/tanaris/numbers.test.js
+++ test/tanaris/numbers.test.js
@@ -4,7 +4,15 @@ import { describe, test, expect } from "@jest/globals";
 
 import { tableTestName, testTimeoutMs, toTable } from "/tanaris/testing/jest";
 
-import { isBigInt, isInteger, isNumber } from "/tanaris/numbers";
+import {
+  isBigInt,
+  isFinite,
+  isInfinite,
+  isInteger,
+  isNaN,
+  isNumber,
+  isNumberLike,
+} from "/tanaris/numbers";
 
 // prettier-ignore
 const bigInts = toTable([
@@ -27,22 +35,69 @@ const integers = nonBigIntIntegers.concat(bigInts);
 // prettier-ignore
 const nonIntegerNumbers = toTable([
   1.1,
-  NaN,
-  Infinity,
-  -Infinity,
+  -4.67,
+  -2/3,
+  100000.00000000001,
 ]);
 
 const numbers = integers.concat(nonIntegerNumbers);
 
 // prettier-ignore
-const nonNumbers = toTable([
+const valuesUnrelatedToNumbers = toTable([
+  "27",
+  "27.27",
+  "27,5",
+  "123abc",
   null,
   undefined,
+  false,
   true,
   Symbol("bar"),
   {},
-  []
+  [],
+  [1],
+  [1, 2],
+  new Date(),
+  new Date().toString(),
 ]);
+
+// prettier-ignore
+const nonNumbers = toTable([
+  Infinity,
+  -Infinity,
+  NaN,
+]).concat(valuesUnrelatedToNumbers);
+
+describe("calling `isNumberLike/1`", () => {
+  describe("on a number returns `true`", () => {
+    test.each(numbers)(
+      tableTestName,
+      (value) => {
+        expect(isNumberLike(value)).toBe(true);
+      },
+      testTimeoutMs
+    );
+  });
+
+  test("on +/- `Infinity` returns `true`", () => {
+    expect(isNumberLike(Infinity)).toBe(true);
+    expect(isNumberLike(-Infinity)).toBe(true);
+  });
+
+  test("on `NaN` returns `true`", () => {
+    expect(isNumberLike(NaN)).toBe(true);
+  });
+
+  describe("on a value unrelated to numbers returns `false`", () => {
+    test.each(valuesUnrelatedToNumbers)(
+      tableTestName,
+      (value) => {
+        expect(isNumberLike(value)).toBe(false);
+      },
+      testTimeoutMs
+    );
+  });
+});
 
 describe("calling `isNumber/1`", () => {
   describe("on a number returns `true`", () => {
@@ -134,6 +189,91 @@ describe("calling `isBigInt/1`", () => {
       tableTestName,
       (value) => {
         expect(isNumber(value)).toBe(false);
+      },
+      testTimeoutMs
+    );
+  });
+});
+
+describe("calling `isFinite/1`", () => {
+  test("on +/- `Infinity` returns `false`", () => {
+    expect(isFinite(Infinity)).toBe(false);
+    expect(isFinite(-Infinity)).toBe(false);
+  });
+
+  describe("on a number returns `true`", () => {
+    test.each(numbers)(
+      tableTestName,
+      (value) => {
+        expect(isFinite(value)).toBe(true);
+      },
+      testTimeoutMs
+    );
+  });
+
+  describe("on a non-number returns `false`", () => {
+    test.each(nonNumbers)(
+      tableTestName,
+      (value) => {
+        expect(isFinite(value)).toBe(false);
+      },
+      testTimeoutMs
+    );
+  });
+});
+
+describe("calling `isInfinite/1`", () => {
+  test("on +/- `Infinity` returns `true`", () => {
+    expect(isInfinite(Infinity)).toBe(true);
+    expect(isInfinite(-Infinity)).toBe(true);
+  });
+
+  describe("on a number returns `false`", () => {
+    test.each(numbers)(
+      tableTestName,
+      (value) => {
+        expect(isInfinite(value)).toBe(false);
+      },
+      testTimeoutMs
+    );
+  });
+
+  describe("on a value unrelated to numbers returns `false`", () => {
+    test.each(valuesUnrelatedToNumbers)(
+      tableTestName,
+      (value) => {
+        expect(isInfinite(value)).toBe(false);
+      },
+      testTimeoutMs
+    );
+  });
+});
+
+describe("calling `isNaN/1`", () => {
+  test("on `NaN` returns `true`", () => {
+    expect(isNaN(NaN)).toBe(true);
+  });
+
+  describe("on a number returns `false`", () => {
+    test.each(numbers)(
+      tableTestName,
+      (value) => {
+        expect(isNaN(value)).toBe(false);
+      },
+      testTimeoutMs
+    );
+  });
+
+  test("on +/- `Infinity` returns `false`", () => {
+    expect(isNaN(Infinity)).toBe(false);
+    expect(isNaN(-Infinity)).toBe(false);
+  });
+
+  describe("on a value unrelated to numbers returns `false`", () => {
+    test.each(valuesUnrelatedToNumbers)(
+      tableTestName,
+      (value) => {
+        expect(isNaN(value)).toBe(false);
       },
       testTimeoutMs
     );
